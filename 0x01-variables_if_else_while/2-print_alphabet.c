013ab87e5e1f545c15f3f59a1f71022a6cf3fed3
TSHABBA01 <tshabalala.bdd@gmail.com>
2022-09-09 15:30:15 -0700

    assign negayive, zero, or positive varible to random number n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print alphabet in order
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
