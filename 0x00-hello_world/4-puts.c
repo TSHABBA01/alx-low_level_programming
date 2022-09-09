050ad529c5d4c02c79e9332fc0fa35046ae72072
TSHABBA01 <tshabalala.bdd@gmail.com>
2022-09-08 19:25:57 -0700

    Write a C progrm with a printed message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
+ * main - main block
  * Description: A code that displays a message
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like buildng a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
