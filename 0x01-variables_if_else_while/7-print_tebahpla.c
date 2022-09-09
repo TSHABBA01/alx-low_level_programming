7108713f57e35c21af84af3ca1ce304d595b246a
TSHABBA01 <tshabalala.bdd@gmail.com>
2022-09-09 16:59:44 -0700

    nice

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - script to rev alphabet
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char r;
+
+	for (r='Z'; r >= 'a'; r--)
+	{
+		putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
