02813f3deb443ccbadef4261a130078c87fca595
TSHABBA01 <tshabalala.bdd@gmail.com>
2022-09-09 19:42:40 -0700

    prints all possibilities for 3 number didits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,24 +11,27 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		for (i = '0'; k <= '9'; k++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			if (c < i && i < k)
+			for (k = '0'; k <= '9'; k++)
 			{
-				putchar(c);
-				putchar(i);
-				putchar(k);
-
-				if (c != '7')
+				if (c < i && i < k)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(c);
+					putchar(i);
+					putchar(k);
+					
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
 	}
-
+	
 	putchar('\n');
-
 	return (0);
 }
+	
