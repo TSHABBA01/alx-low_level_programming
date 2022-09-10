069ed0fcdfcd1065db10c590c5249974dc8cfb56
TSHABBA01 <tshabalala.bdd@gmail.com>
2022-09-09 19:06:07 -0700

    print all possible different combination of three numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main -main block
+ *Description: prints all possible combinations of three digits
+ *Return: 0
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
