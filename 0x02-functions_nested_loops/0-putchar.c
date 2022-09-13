53395c1f7d007b4f46775d6f6e3b1d57b5cdb754
TSHABBA01 <tshabalala.bdd@gmail.com>
2022-09-13 15:45:36 -0700

    assign negayive, zero, or positive varible to random number n

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Description: a program that prints putchar.c
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char text[] = "_putchar";
+	int w = 0;
+
+	while (text[w] != '\0')
+	{
+		putchar(text[w]);
+		w++;
+	}
+	putchar('\n');
+		return (0);
+}
