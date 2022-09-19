74017716d99eb1fae033a3fc41f958b4274fbbd0
TSHABBA01 <tshabalala.bdd@gmail.com>
2022-09-19 14:14:20 -0700

    sly

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *strlen - returns the length of a string
- *@s: number tested
- *Return: length of string
+ * _strlen - returns the length of a string
+ * @s: number tested
+ * Return: length of string
  */
 int _strlen(char *s)
 {
 int count = 0, i = 0;
-while(s[i] != '\0')
+while (s[i] != '\0')
 {
 count++;
 i++;
